ab4cc2502902b35dadccc81885928aa1e67cad49
Florian Jerrige <fjerrige@f6r9s5.clusters.42paris.fr>
2026-04-07 16:56:25 +0200

    test

--- src/syntax_check.c
+++ src/syntax_check.c
@@ -12,6 +12,27 @@
 
 #include "minishell.h"
 
+void	print_message(char *s)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd("syntax error near unexpected token `", 2);
+	ft_putstr_fd(s, 2);
+	ft_putstr_fd("'\n", 2);
+}
+
+int	handle_double_redirs(t_token *tmp)
+{
+	if (tmp->next->type == IN_DIR)
+		return (print_message("<"), 0);
+	else if (tmp->next->type == OUT_DIR)
+		return (print_message(">"), 0);
+	else if (tmp->next->type == HEREDOC)
+		return (print_message("<<"), 0);
+	else if (tmp->next->type == APPEND)
+		return (print_message(">>"), 0);
+	return (1);
+}
+
 int	syntax_check(t_data *data)
 {
 	t_token	*tmp;
@@ -20,28 +41,22 @@ int	syntax_check(t_data *data)
 	if (!tmp)
 		return (1);
 	if (tmp->type == PIPE)
-		return (f_printf("syntax error near unexpected token `|'\n", NULL), 0);
+		return (print_message("|"), 0);
 	while (tmp)
 	{
 		if (tmp->type == PIPE && tmp->next && tmp->next->type == PIPE)
-			return (f_printf("syntax error near unexpected token `|'\n", NULL), 0);
+			return (print_message("|"), 0);
 		else if (tmp->type == PIPE && !tmp->next)
-			return (f_printf("syntax error near unexpected token `newline'\n", NULL), 0);
+			return (print_message("newline"), 0);
 		else if (is_redir(tmp->type) && tmp->next && is_redir(tmp->next->type))
 		{
-			if (tmp->next->type == IN_DIR)
-				return (f_printf("syntax error near unexpected token `<'\n", NULL), 0);
-			else if (tmp->next->type == OUT_DIR)
-				return (f_printf("syntax error near unexpected token `>'\n", NULL), 0);
-			else if (tmp->next->type == HEREDOC)
-				return (f_printf("syntax error near unexpected token `<<'\n", NULL), 0);
-			if (tmp->next->type == APPEND)
-				return (f_printf("syntax error near unexpected token `>>'\n", NULL), 0);
+			if (!handle_double_redirs(tmp))
+				return (0);
 		}
 		else if (is_redir(tmp->type) && tmp->next && tmp->next->type == PIPE)
-			return (f_printf("syntax error near unexpected token `|'\n", NULL), 0);
+			return (print_message("|"), 0);
 		else if (is_redir(tmp->type) && !tmp->next)
-			return (f_printf("syntax error near unexpected token `newline'\n", NULL), 0);
+			return (print_message("newline"), 0);
 		tmp = tmp->next;
 	}
 	return (1);
